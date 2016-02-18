ab09bbf7838ccd5fcca19cb45995dbdbd40c51b3
hnoslen <hnoslen@users.noreply.github.com>
2016-02-17 23:09:53 -0600

    Initial version

---
+++ mazeLearn.py
@@ -0,0 +1,10 @@
+# implement R Learn
+import RLearn
+albert = RLearn.Agent(11)
+conex = [[0,1],[1,2],[1,4],[2,3],[2,5],[3,6],[4,5],[4,7],[5,6],[5,8],[7,8],[8,9],[9,10]]
+albert.end_states = [10]
+albert.reward_states = [10]
+albert.punish_states = [6]
+print albert.multiTransition(conex, False)
+albert.startWeights()
+end_conds = albert.batchRun(1000)
